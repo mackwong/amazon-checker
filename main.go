caa52ee0da2588803d78e6077941a4e7634ec0f8
Wang Jun <mail@mack.wang>
2020-02-22 00:32:14 +0800

    init

--- main.go
+++ main.go
@@ -9,6 +9,9 @@ import (
 	"fyne.io/fyne/layout"
 	"fyne.io/fyne/widget"
 	"image/color"
+	"io/ioutil"
+	"net/http"
+	"strings"
 	"time"
 )
 
@@ -18,13 +21,19 @@ var (
 	foundText        *canvas.Text
 	entry            *widget.Entry
 	count            = 0
-	found            = 0
 	cancelFunc       context.CancelFunc
 )
 
 func start() {
+	if !strings.HasPrefix(entry.Text, "https://www.amazon.co.jp/") {
+		foundText.Text = "url is not amazon japan!"
+		refresh()
+		return
+	}
 	var ctx context.Context
 	ctx, cancelFunc = context.WithCancel(context.Background())
+	reset()
+	refresh()
 	go run(ctx)
 	startBtn.Disable()
 	entry.Disable()
@@ -38,11 +47,26 @@ func run(ctx context.Context) {
 			return
 		default:
 			count++
+			s, has, err := crawl(entry.Text)
+			if err != nil {
+				foundText.Text = err.Error()
+				foundText.Refresh()
+				time.Sleep(5 * time.Second)
+				continue
+			}
+			foundText.Text = s
+			if has {
+				startBtn.Enable()
+				entry.Enable()
+				endBtn.Disable()
+				foundText.Refresh()
+				refresh()
+				return
+			}
 			refresh()
 			time.Sleep(5 * time.Second)
 		}
 	}
-
 }
 
 func end() {
@@ -55,19 +79,41 @@ func end() {
 }
 
 func reset() {
+	foundText.Text = "No found"
 	count = 0
+}
+
+func crawl(url string) (string, bool, error) {
+	client := http.Client{
+		Timeout: 10 * time.Second,
+	}
+	resp, err := client.Get(url)
+	if resp != nil {
+		defer resp.Body.Close()
+	}
+	if err != nil {
+		return "", false, err
+	}
+	c, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		return "", false, err
+	}
+	content := string(c)
+	if strings.Contains(content, "buy-now-button") {
+		return "You can buy!!", true, nil
+	}
+	return "Out of stock", false, nil
 }
 
 func refresh() {
 	checkTimes.Text = fmt.Sprintf("Checking: %d", count)
-	foundText.Text = fmt.Sprintf("Found: %d", found)
 	checkTimes.Refresh()
 	foundText.Refresh()
 }
 
 func Render() *fyne.Container {
 	checkTimes = canvas.NewText(fmt.Sprintf("Checking: %d", count), color.White)
-	foundText = canvas.NewText(fmt.Sprintf("Found: %d", found), color.RGBA{0, 255, 0, 0})
+	foundText = canvas.NewText("No found", color.RGBA{0, 255, 0, 0})
 	foundText.TextSize = 30
 	foundText.Alignment = fyne.TextAlignCenter
 	checkTimes.TextSize = 30
@@ -92,6 +138,7 @@ func main() {
 	myWindow := myApp.NewWindow("Amazon Checker")
 	content := Render()
 	myWindow.Resize(fyne.NewSize(400, 600))
+	myWindow.SetFixedSize(true)
 	myWindow.SetContent(content)
 	myWindow.ShowAndRun()
 }
